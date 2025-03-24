78eb2714bee1c3b23b83be8d0fd25be61ea3b4ef
Alan <161715146+AlanPierree@users.noreply.github.com>
2025-03-24 11:06:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ document.getElementById("Confirmar").addEventListener("click", function () {
 
     // Mensagens para cada autor
     const mensagens = {
-        "agatha christie": "jlnld",
-        "arthur doyle": "",
-        "edgar poe": "",
-        "shakespeare": "",
-        "rick riordan": "",
-        "rowling": "",
-        "tolkien": "",
+        "agatha christie": "Assassinato no Expresso do Oriente; Morte no Nilo; E Não Sobrou Nenhum. ",
+        "arthur doyle": "Sherlock Holmes (56 histórias); O Mundo Perdido.",
+        "edgar poe": "O Corvo; O Gato Preto; Os Assassinatos da Rua Morgue.",
+        "shakespeare": "Romeu e Julieta; Hamlet; Macbeth.",
+        "rick riordan": "Percy Jackson e os Olimpianos; Os Heróis do Olimpo; Magnus Chase e os Deuses de Asgard.",
+        "rowling": "Saga Harry Potter (7 livros); Animais Fantásticos.",
+        "tolkien": "Senhor dos Anéis; O Hobbit.",
     }
 
     // Buscar a mensagem para o autor selecionado
